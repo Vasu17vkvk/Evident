09e4a5550d3575bbd1d316d214843b034d50fe94
Vasudev VK <vasudevvk45@gmail.com>
2026-07-11 01:39:17 +0530

    Fix edit keys

--- src/services/upload/uploadService.ts
+++ src/services/upload/uploadService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL ||
+    "https://evident-0e7j.onrender.com";
 
 export const requestUploadUrl = async (
     filename: string,
