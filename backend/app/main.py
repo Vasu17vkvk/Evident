5228e63d086ab431009ed953ad8bcb6b100f7d99
Vasudev VK <vasudevvk45@gmail.com>
2026-07-10 22:33:35 +0530

    Evident AI MVP v2

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://evident-murex.vercel.app/"
 ]
 
 app.add_middleware(
@@ -62,4 +63,4 @@ async def ai_test():
         raise HTTPException(
             status_code=500,
             detail=f"Gemini Test Failed: {str(e)}"
-        )
+        )
